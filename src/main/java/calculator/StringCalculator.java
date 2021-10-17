88fcc15b85052630a99df014d94858f45dee56c9
Pranita Jadhav <55285423+prani26@users.noreply.github.com>
2021-10-17 11:48:03 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,6 +3,11 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+        private float result;
+        private String delimiters;
+        private static final DEFAULT_DELIMITER=",";
+        
+        
         return 0;
     }
 
